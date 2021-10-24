194f3305127b2cbcfcb5531bbf7e76d04fa84c9d
Potato <silenceace@gmail.com>
2021-10-24 14:28:11 +0800

    chore: 优化

---
+++ .prettierrc.js
@@ -0,0 +1,15 @@
+// https://prettier.io/docs/en/index.html
+module.exports = {
+    // 尾后逗号
+    trailingComma: 'es5',
+    tabWidth: 4,
+    // 结尾分号
+    semi: false,
+    // Use single quotes instead of double quotes.
+    singleQuote: true,
+    // Change when properties in objects are quoted.
+    quoteProps: 'consistent',
+    bracketSpacing: true,
+    bracketSameLine: false,
+
+}
